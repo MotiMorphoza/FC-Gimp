492df026c10719829a7f1ada1e78e71f71ca797d
MotiMorphoza <mottex@gmail.com>
2026-02-19 20:20:45 +0200

    Update layout.9715c738.js

--- docs/js/layout.9715c738.js
+++ docs/js/layout.9715c738.js
@@ -75,7 +75,7 @@ async function loadProject() {
 document.addEventListener("DOMContentLoaded", async () => {
 
   await loadSidebar();
-  await loadProjectFromJSON();
+  await loadProject();
 
   const images = document.querySelectorAll(".project-gallery img");
   if (!images.length) return;
